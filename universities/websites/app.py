007cfd7aec381fa667112f930c595923d228dbdc
lavskillup <lavanyas@skillup.tech>
2023-02-08 16:07:10 +0530

    Adding the initial files

---
+++ universities/websites/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_cors import CORS
+import json
+
+app = Flask("University Websites")
+CORS(app)
+
+with open("UK_Universities.json", "r") as unifile:
+    data = json.load(unifile)
+
+
+@app.route("/websites/<name>")
+def getCollegesWebsites(name):
+    webpages = []
+    for college in data:
+        if college["name"].__contains__(name):
+            for website in college["web_pages"]:
+                webpages.append(website)
+    return {"Webpages": webpages}
+
+
+# @app.route("/websites/<name>")
+# def getCollegesWebsites(name):
+#     webpages = []
+#     for college in data:
+#         if college["name"] == name:
+#             for website in college["web_pages"]:
+#                 webpages.append(website)
+#     return {"Webpages": webpages}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
